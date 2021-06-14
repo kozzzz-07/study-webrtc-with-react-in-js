225aed17e6844187f5c8851da75d4bd4ccf2dd44
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2021-06-14 14:50:23 +0900

    permit audio and video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
-function App() {
+const getMedia = async () => {
+  const constraints = { audio: true, video: true };
+  try {
+    // ブラウザで許可を求める通知が出る
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getMedia();
+
+const App = () => {
   return <div>Hello, React!</div>;
-}
+};
 
 export default App;
